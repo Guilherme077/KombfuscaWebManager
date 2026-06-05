600e2ee3c8d171faae9efd4e6b3aa310f83d3b2a
Guilherme077 <gui.pilz.2007@gmail.com>
2026-06-05 11:15:16 -0300

    Adicionar arquivos de projeto.

---
+++ KombfuscaWebManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KombfuscaWebManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
