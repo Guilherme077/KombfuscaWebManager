5813afabab5782bb92bae49a99ad76c0a7ed0ec0
Guilherme077 <gui.pilz.2007@gmail.com>
2026-06-23 10:52:00 -0300

    Feat: The scoresheets (to print) are created according to number of counters

--- KombfuscaWebManager/Controllers/DocsController.cs
+++ KombfuscaWebManager/Controllers/DocsController.cs
@@ -30,11 +30,13 @@ namespace KombfuscaWebManager.Controllers
         [Authorize(Roles = Roles.Admin)]
         public IActionResult GenerateScoreSheets(int cupId, string footer)
         {
-            var cup = _context.Cups.Where(c => c.Id == cupId).Include(c => c.Periods).FirstOrDefault();
+            var cup = _context.Cups.Where(c => c.Id == cupId).Include(c => c.Periods).Include(c => c.Assignments).FirstOrDefault();
 
             int nPages = cup.Periods.Count;
             string gameName = cup.Name;
             string[] periodsDesc = cup.Periods.Select(p => p.Description).ToArray();
+
+            var counters = cup.Assignments.ToArray();
 
             string modelPath = Path.Combine(_env.ContentRootPath, "Services", "PdfModels", "scoreSheetModel.pdf");
             string coverModelPath = Path.Combine(_env.ContentRootPath, "Services", "PdfModels", "coverScoreSheetModel.pdf");
@@ -53,88 +55,30 @@ namespace KombfuscaWebManager.Controllers
                 PdfWriter writerFinal = new PdfWriter(streamFinal);
                 PdfDocument pdfDest = new PdfDocument(writerFinal);
 
-
-                using (MemoryStream streamTemp = new MemoryStream())
-                {
-                    PdfReader readerModel = new PdfReader(coverModelPath);
-                    PdfWriter writerTemp = new PdfWriter(streamTemp);
-                    PdfDocument pdfTemp = new PdfDocument(readerModel, writerTemp);
-
-                    Document docLayout = new Document(pdfTemp);
-                    PdfFont fontObj = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
-
-                    Paragraph text = new Paragraph(gameName).SetFont(fontObj).SetFontSize(16).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
-
-                    float posX = 0;
-                    float posY = 420;
-
-                    text.SetFixedPosition(posX, posY, 595);
-
-                    docLayout.Add(text);
-
-                    docLayout.Close();
-                    pdfTemp.Close();
-
-
-                    using (MemoryStream streamLeituraTemp = new MemoryStream(streamTemp.ToArray()))
-                    {
-                        PdfReader readerPaginaPronta = new PdfReader(streamLeituraTemp);
-                        PdfDocument pdfOriginalReady = new PdfDocument(readerPaginaPronta);
-
-                        pdfOriginalReady.CopyPagesTo(1, 1, pdfDest);
-
-                        pdfOriginalReady.Close();
-                    }
-                }
-
-
-                for (int i = 1; i <= nPages; i++)
+                foreach (var counter in counters)
                 {
                     using (MemoryStream streamTemp = new MemoryStream())
                     {
-                        PdfReader readerModel = new PdfReader(modelPath);
+                        PdfReader readerModel = new PdfReader(coverModelPath);
                         PdfWriter writerTemp = new PdfWriter(streamTemp);
                         PdfDocument pdfTemp = new PdfDocument(readerModel, writerTemp);
 
                         Document docLayout = new Document(pdfTemp);
-                        PdfFont fontObj = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+                        PdfFont fontObj = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
 
-                        //Page Number and period desc
+                        Paragraph title = new Paragraph(gameName).SetFont(fontObj).SetFontSize(16).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
 
-                        Paragraph numberText = new Paragraph(i.ToString() + " | " + periodsDesc[i-1]).SetFont(fontObj).SetFontSize(10).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.RIGHT);
+                        float posX = 0;
+                        float posY = 420;
 
-                        float posX = 480;
-                        float posY = 775;
+                        title.SetFixedPosition(posX, posY, 595);
 
-                        numberText.SetFixedPosition(posX, posY, 75); // 75 is the max length
-
-                        docLayout.Add(numberText);
-
-                        //Footer
-                        Paragraph footerText = new Paragraph(footer).SetFont(fontObj).SetFontSize(10).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
-
-                        posX = 0;
-                        posY = 35;
-
-                        footerText.SetFixedPosition(posX, posY, 595); // 595 is the length of A4 paper
-
-                        docLayout.Add(footerText);
-
-                        //Game Name
-                        fontObj = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
-                        Paragraph gameNameText = new Paragraph(gameName).SetFont(fontObj).SetFontSize(14).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
-
-                        posX = 80;
-                        posY = 775;
-
-                        gameNameText.SetFixedPosition(posX, posY, 450); // 450 is the max length (530-80)
-
-                        docLayout.Add(gameNameText);
+                        docLayout.Add(title);
 
                         docLayout.Close();
                         pdfTemp.Close();
 
-                        
+
                         using (MemoryStream streamLeituraTemp = new MemoryStream(streamTemp.ToArray()))
                         {
                             PdfReader readerPaginaPronta = new PdfReader(streamLeituraTemp);
@@ -143,6 +87,66 @@ namespace KombfuscaWebManager.Controllers
                             pdfOriginalReady.CopyPagesTo(1, 1, pdfDest);
 
                             pdfOriginalReady.Close();
+                        }
+                    }
+
+
+                    for (int i = 1; i <= nPages; i++)
+                    {
+                        using (MemoryStream streamTemp = new MemoryStream())
+                        {
+                            PdfReader readerModel = new PdfReader(modelPath);
+                            PdfWriter writerTemp = new PdfWriter(streamTemp);
+                            PdfDocument pdfTemp = new PdfDocument(readerModel, writerTemp);
+
+                            Document docLayout = new Document(pdfTemp);
+                            PdfFont fontObj = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
+                            //Page Number and period desc
+
+                            Paragraph numberText = new Paragraph(i.ToString() + " | " + periodsDesc[i - 1]).SetFont(fontObj).SetFontSize(10).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.RIGHT);
+
+                            float posX = 480;
+                            float posY = 775;
+
+                            numberText.SetFixedPosition(posX, posY, 75); // 75 is the max length
+
+                            docLayout.Add(numberText);
+
+                            //Footer
+                            Paragraph footerText = new Paragraph(footer).SetFont(fontObj).SetFontSize(10).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
+
+                            posX = 0;
+                            posY = 35;
+
+                            footerText.SetFixedPosition(posX, posY, 595); // 595 is the length of A4 paper
+
+                            docLayout.Add(footerText);
+
+                            //Game Name
+                            fontObj = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+                            Paragraph gameNameText = new Paragraph(gameName).SetFont(fontObj).SetFontSize(14).SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
+
+                            posX = 80;
+                            posY = 775;
+
+                            gameNameText.SetFixedPosition(posX, posY, 450); // 450 is the max length (530-80)
+
+                            docLayout.Add(gameNameText);
+
+                            docLayout.Close();
+                            pdfTemp.Close();
+
+
+                            using (MemoryStream streamLeituraTemp = new MemoryStream(streamTemp.ToArray()))
+                            {
+                                PdfReader readerPaginaPronta = new PdfReader(streamLeituraTemp);
+                                PdfDocument pdfOriginalReady = new PdfDocument(readerPaginaPronta);
+
+                                pdfOriginalReady.CopyPagesTo(1, 1, pdfDest);
+
+                                pdfOriginalReady.Close();
+                            }
                         }
                     }
                 }
