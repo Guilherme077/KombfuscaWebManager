270a2f1200cbbd8df66ef92f5428e2bb9c6c71d6
Guilherme077 <gui.pilz.2007@gmail.com>
2026-08-02 21:11:06 -0300

    Refine player area and score views

--- KombfuscaWebManager/Controllers/HomeController.cs
+++ KombfuscaWebManager/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 using KombfuscaWebManager.Data;
 using KombfuscaWebManager.Models;
 using KombfuscaWebManager.Models.CupModels;
+using KombfuscaWebManager.Models.CupModels.ViewModels;
+using KombfuscaWebManager.Models.SystemModels.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace KombfuscaWebManager.Controllers
 {
@@ -36,8 +39,40 @@ namespace KombfuscaWebManager.Controllers
         }
         public async Task<IActionResult> PlayerArea()
         {
-            var cups = await _context.Cups.Where(c => c.cupStatus == CupStatus.openSubscriptions).ToListAsync();
-            return View(cups);
+            string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null) return Unauthorized();
+
+            var cupsToSub = await _context.Cups.Where(c => c.cupStatus == CupStatus.openSubscriptions).ToListAsync();
+            var cupsParticipated = await _context.CupResults.Where(c => c.UserId == userId).ToListAsync();
+
+            var userCups = new List<MyScoreViewModel>();
+
+            foreach (var cupResult in cupsParticipated) {
+                var cup = await _context.Cups.FirstOrDefaultAsync(c => c.Id == cupResult.CupId);
+                userCups.Add(new MyScoreViewModel
+                {
+                    TeamName = cupResult.TeamName,
+                    TotalScore = cupResult.TotalScore,
+                    Position = cupResult.Position,
+                    CupId = cup.Id,
+                    CupName = cup.Name,
+                    CupYear = cup.StartDate.Year,
+                });
+            }
+
+            var numberCups = await _context.CupResults.Where(c => c.UserId == userId).CountAsync();
+            var numberVictory = await _context.CupResults.Where(c => c.UserId == userId && c.Position == 1).CountAsync();
+
+            var vm = new PlayerAreaViewModel()
+            {
+                Victory = numberVictory,
+                CupNumbers = numberCups,
+                UserCups = userCups,
+                CupsToSubscription = cupsToSub
+            };
+
+            return View(vm);
         }
         public IActionResult ScoreCounterArea()
         {
---
+++ KombfuscaWebManager/Models/SystemModels/ViewModels/PlayerAreaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using KombfuscaWebManager.Models.CupModels;
+using KombfuscaWebManager.Models.CupModels.ViewModels;
+
+namespace KombfuscaWebManager.Models.SystemModels.ViewModels
+{
+    public class PlayerAreaViewModel
+    {
+        public int Victory { get; set; }
+        public int CupNumbers { get; set; }
+        public List<MyScoreViewModel> UserCups { get; set; }
+        public List<Cup> CupsToSubscription { get; set; }
+    }
+}
