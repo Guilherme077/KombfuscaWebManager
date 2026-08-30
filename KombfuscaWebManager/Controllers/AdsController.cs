acd25afc3c715a0bf1629b20ff25e314464f1b35
Guilherme077 <123081093+Guilherme077@users.noreply.github.com>
2026-08-30 17:57:01 -0300

    AdPeriod creation validation

--- KombfuscaWebManager/Controllers/AdsController.cs
+++ KombfuscaWebManager/Controllers/AdsController.cs
@@ -21,7 +21,15 @@ namespace KombfuscaWebManager.Controllers
 
         public IActionResult Index()
         {
-            return RedirectToAction("AdsCentral");
+            if(User.Identity.IsAuthenticated)
+            {
+                if(User.IsInRole(Roles.Admin))
+                {
+                    return RedirectToAction("ManageAds");
+                }
+                return RedirectToAction("AdsCentral");
+            }
+            return Unauthorized();
         }
 
         [Authorize]
@@ -43,9 +51,63 @@ namespace KombfuscaWebManager.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateAdPeriod(AdSubscriptionPeriod adPeriod)
         {
+            var hasInvalidDate = false;
+
+            if (adPeriod.EndSubscription <= adPeriod.StartSubscription)
+            {
+                ModelState.AddModelError(string.Empty,
+                    "A data final das inscrições deve ser posterior à data inicial.");
+                hasInvalidDate = true;
+            }
+
+            if (adPeriod.SituationReviewDate <= adPeriod.EndSubscription)
+            {
+                ModelState.AddModelError(string.Empty,
+                    "A data de liberação dos resultados de inscrições deve ser posterior ao final das inscrições.");
+                hasInvalidDate = true;
+            }
+
+            if (adPeriod.StartAuction <= adPeriod.SituationReviewDate)
+            {
+                ModelState.AddModelError(string.Empty,
+                    "O período de leilão deve ser posterior à data de liberação de resultados.");
+                hasInvalidDate = true;
+            }
+
+            if (adPeriod.EndAuction <= adPeriod.StartAuction)
+            {
+                ModelState.AddModelError(string.Empty,
+                    "A data final do leilão deve ser posterior à data inicial.");
+                hasInvalidDate = true;
+            }
+
             var cup = await _context.Cups.Where(c => c.StartDate >= DateTime.Now).OrderBy(c => c.StartDate).FirstOrDefaultAsync();
             if (cup == null) return BadRequest("No upcoming cups found.");
             var cupId = cup.Id;
+
+            var existingPeriodsConflict = await _context.AdSubscriptionPeriods.Where(p => p.CupId == cupId).ToListAsync();
+
+            if(existingPeriodsConflict.Count > 0)
+            {
+                ModelState.AddModelError(string.Empty, "A copa mais próxima já possui um período de anúncios configurado.");
+                hasInvalidDate = true;
+            }
+
+            if (hasInvalidDate)
+            {
+                var adPeriods = await _context.AdSubscriptionPeriods
+                    .Include(p => p.Cup)
+                    .Include(p => p.Categories)
+                    .ToListAsync();
+
+                ViewData["StartSubscription"] = adPeriod.StartSubscription.ToString("yyyy-MM-dd");
+                ViewData["EndSubscription"] = adPeriod.EndSubscription.ToString("yyyy-MM-dd");
+                ViewData["SituationReviewDate"] = adPeriod.SituationReviewDate.ToString("yyyy-MM-dd");
+                ViewData["StartAuction"] = adPeriod.StartAuction.ToString("yyyy-MM-dd");
+                ViewData["EndAuction"] = adPeriod.EndAuction.ToString("yyyy-MM-dd");
+
+                return View(nameof(ManageAds), adPeriods);
+            }
 
             var newPeriod = new AdSubscriptionPeriod
             {
